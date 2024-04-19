b4753d858da837028c365ca7b59a97801c4f15f0
Hemvyas <hem.vyas09@gmail.com>
2024-04-19 19:52:37 +0530

    .

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,24 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import {Auth0Provider} from "@auth0/auth0-react"
-import { MantineProvider } from "@mantine/core";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-    >
       <Auth0Provider
-        domain="dev-satxjv8si61r32f1.us.auth0.com"
-        clientId="PWEcd7yE0nvqEYs91PYM0sC3Jf9wr2XB"
+        domain={import.meta.env.VITE_APP_DOMAIN}
+        clientId={import.meta.env.VITE_APP_CLIENT_ID}
         authorizationParams={{
           redirect_uri: "http://localhost:5173",
         }}
-        audience="https://dev-satxjv8si61r32f1.us.auth0.com/api/v2/"
+        audience={import.meta.env.VITE_APP_AUDIENCE}
         scope="openid profile email"
       >
         <App />
       </Auth0Provider>
-    </MantineProvider>
   </React.StrictMode>
 );
