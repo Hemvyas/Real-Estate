468a3e19a2dc92c8e86ae6ecf1d6140213c65795
Hemvyas <hem.vyas09@gmail.com>
2024-04-19 20:43:31 +0530

    autho url

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,16 +5,16 @@ import App from "./App";
 import {Auth0Provider} from "@auth0/auth0-react"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <Auth0Provider
-        domain={import.meta.env.VITE_APP_DOMAIN}
-        clientId={import.meta.env.VITE_APP_CLIENT_ID}
-        authorizationParams={{
-          redirect_uri: "http://localhost:5173",
-        }}
-        audience={import.meta.env.VITE_APP_AUDIENCE}
-        scope="openid profile email"
-      >
-        <App />
-      </Auth0Provider>
+    <Auth0Provider
+      domain={import.meta.env.VITE_APP_DOMAIN}
+      clientId={import.meta.env.VITE_APP_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: "https://real-estate-iota-seven.vercel.app/",
+      }}
+      audience={import.meta.env.VITE_APP_AUDIENCE}
+      scope="openid profile email"
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
